966f01048fbda7b82d416aad5e52051df5fac97a
makenaJun <makena.jun@gmail.com>
2022-08-01 22:45:51 +0300

    Fix validation v4

--- src/validators/bloggersValidator.ts
+++ src/validators/bloggersValidator.ts
@@ -10,13 +10,15 @@ export const bloggersValidator = {
         check(['name'])
             .isString()
             .withMessage('Invalid data type passed')
-            .isLength({max: 15})
-            .withMessage('Max 15 symbols'),
+            .trim()
+            .isLength({min: 1, max: 15})
+            .withMessage('Min 1, Max 15 symbols'),
         check(['youtubeUrl'])
             .isURL()
             .withMessage('Not correct url')
-            .isLength({max: 100})
-            .withMessage('Max 100 symbols'),
+            .trim()
+            .isLength({min: 1, max: 100})
+            .withMessage('Min 1, Max 100 symbols'),
     ],
     "UPDATE_BLOGGER": [
         check(['id'])
@@ -25,13 +27,15 @@ export const bloggersValidator = {
         check(['name'])
             .isString()
             .withMessage('Invalid data type passed')
-            .isLength({max: 15})
-            .withMessage('Max 15 symbols'),
+            .trim()
+            .isLength({min: 1, max: 15})
+            .withMessage('Min 1, Max 15 symbols'),
         check(['youtubeUrl'])
             .isURL()
             .withMessage('Not correct url')
-            .isLength({max: 100})
-            .withMessage('Max 100 symbols'),
+            .trim()
+            .isLength({min: 1, max: 100})
+            .withMessage('Min 1, Max 100 symbols'),
     ],
     "DELETE_BLOGGER": [
         check(['id'])
--- src/validators/postsValidator.ts
+++ src/validators/postsValidator.ts
@@ -10,18 +10,21 @@ export const postsValidator = {
         check(['title'])
             .isString()
             .withMessage('Invalid data type passed')
-            .isLength({max: 30})
-            .withMessage('Max 30 symbols'),
+            .trim()
+            .isLength({min: 1,max: 30})
+            .withMessage('Min 1, Max 30 symbols'),
         check(['shortDescription'])
             .isString()
             .withMessage('Invalid data type passed')
-            .isLength({max: 100})
-            .withMessage('Max 100 symbols'),
+            .trim()
+            .isLength({min: 1,max: 100})
+            .withMessage('Min 1, Max 100 symbols'),
         check(['content'])
             .isString()
             .withMessage('Invalid data type passed')
-            .isLength({max: 1000})
-            .withMessage('Max 1000 symbols'),
+            .trim()
+            .isLength({min: 1,max: 1000})
+            .withMessage('Min 1, Max 1000 symbols'),
         check(['bloggerId'])
             .isNumeric()
             .withMessage('Invalid data type passed')
@@ -30,18 +33,21 @@ export const postsValidator = {
         check(['title'])
             .isString()
             .withMessage('Invalid data type passed')
-            .isLength({max: 30})
-            .withMessage('Max 30 symbols'),
+            .trim()
+            .isLength({min: 1,max: 30})
+            .withMessage('Min 1, Max 30 symbols'),
         check(['shortDescription'])
             .isString()
             .withMessage('Invalid data type passed')
-            .isLength({max: 100})
-            .withMessage('Max 100 symbols'),
+            .trim()
+            .isLength({min: 1,max: 100})
+            .withMessage('Min 1, Max 100 symbols'),
         check(['content'])
             .isString()
             .withMessage('Invalid data type passed')
-            .isLength({max: 1000})
-            .withMessage('Max 1000 symbols'),
+            .trim()
+            .isLength({min: 1,max: 1000})
+            .withMessage('Min 1, Max 1000 symbols'),
         check(['bloggerId'])
             .isNumeric()
             .withMessage('Invalid data type passed')
